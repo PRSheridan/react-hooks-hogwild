2fe71bcc021a596d01a4ea925e21eef8814c5c3a
Phil <philrsheridan@gmail.com>
2024-01-29 16:40:05 -0500

    created Components and outline in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,21 @@ import React from "react";
 import Nav from "./Nav";
 
 import hogs from "../porkers_data";
-
+/*
+Notes
+Components: 
+App: imported list
+Hog: Title card (name and image) + click to display more (specialty, weight, greased, and highest medal achieved) + isclicked?
+HogList: filter hogs greased/not + buttons for sort by name/weight
+FilterHogs: button for filtering by greased or not
+BONUS: hide, add, and semantic cards
+*/
 function App() {
 	return (
 		<div className="App">
 			<Nav />
+			<FilterHog/>
+			<HogList/>
 		</div>
 	);
 }
